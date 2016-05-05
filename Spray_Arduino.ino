c413f17341448f032cde272bc7ef45e54bc6b736
Ali Afshar <alpash1@hotmail.com>
2016-05-05 17:04:11 +0100

    Add ability to receive messages over serial

--- Spray_Arduino.ino
+++ Spray_Arduino.ino
@@ -1,3 +1,7 @@
+//Checks whether the serial message has been fully transcoded
+boolean stringComplete = false;
+String inputString = "";
+
 //The pins of the spray valves
 int sprayGas = 13;  
 int sprayPilot = 12;
@@ -18,6 +22,11 @@ int delayAlkali_Rinse = 1000;
 int delayRinse_Air;
 
 void setup() {
+  //Setup the serial connection
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);  
+
   // initialize digital spray pins as outputs.
   pinMode(sprayGas, OUTPUT);
   pinMode(sprayPilot, OUTPUT);
@@ -28,9 +37,27 @@ void setup() {
 
 
 void loop() {
- runSprayProgram(); 
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    Serial.println(inputString);
+    setSettings(inputString);
+    Serial.println(delayRinse_Air);
+    runSprayProgram(); 
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }  
 }
 
+//Set the settings via the Raspberry Pi
+void setSettings(String settingsString) {
+  sprayTimeQD = settingsString[0] - '0';
+  delayQD_Alkali = settingsString[1] - '0';
+  sprayTimeAlkali = settingsString[2] - '0';
+  delayAlkali_Rinse = settingsString[3] - '0';
+  sprayTimeRinse = settingsString[4] - '0';
+  delayRinse_Air = settingsString[5] - '0';  
+}
 
 //Run the spray program
 void runSprayProgram() {
@@ -57,3 +84,25 @@ void spray(int sprayName, int onTime) {
   delay(onTime);
   digitalWrite(sprayName, LOW);
 }
+
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    } 
+  }
+}
+
